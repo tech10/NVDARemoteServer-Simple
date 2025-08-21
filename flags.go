c5dac6899aa78330c0b3118983c7e01af4f5b8b7
Blake G <blakeblind@gmail.com>
2025-08-21 14:02:26 -0600

    Remove server.pem. Change default for -cert flag to cert.pem from server.pem

--- flags.go
+++ flags.go
@@ -14,7 +14,7 @@ var (
 
 func flags() {
 	flag.StringVar(&addr, "addr", ":6837", "Provide the server with a listening address.")
-	flag.StringVar(&certificatePath, "cert", "server.pem", "Provide the server with a certificate file to load, containing the private key and certificate in .pem format.")
+	flag.StringVar(&certificatePath, "cert", "cert.pem", "Provide the server with a certificate file to load, containing the private key and certificate in .pem format.")
 	flag.BoolVar(&certificateGen, "certgen", false, "Tell the server to automatically generate a certificate. (default false)")
 	flag.BoolVar(&certificateWrite, "certgenwrite", true, "Tell the server to write the generated certificate to the file set in -cert. If you do not write the file to -cert and generate it on launch, you will have a different certificate each time the server launches.")
 	flag.BoolVar(&sendOrigin, "sendorigin", true, "Tell the server to automatically inject an origin field when sending data to a channel. This is required for braille displays to work correctly.")
